e4b77a7642354a17b6d9940017fd8b949360bbda
mac window <mac.window.10@gmail.com>
2021-02-16 01:28:52 +0500

    company edit populate existing

--- basic_app/src/views/Company/EditCompany.js
+++ basic_app/src/views/Company/EditCompany.js
@@ -6,28 +6,22 @@ import ViewCompany from './ViewCompany';
 function EditCompany({ setAlert }) {
     const { id } = useParams();
     const history = useHistory();
-    const [company, setCompany] = useState({
-        name: "",
-        number: "",
-        address: "",
-    });
-
-    const { name, number, address } = company;
-    const onInputChange = e => {
-        setCompany({ ...company, [e.target.name]: e.target.value });
-    };
+    const [itemName, setItemName] = useState();
+    const [itemNumber, setItemNumber] = useState();
+    const [itemAddress, setItemAddress] = useState();
 
     useEffect(() => {
         getCompany(id)
             .then(items => {
-                console.log(items);
-                setCompany(items)
+                setItemName(items.Name);
+                setItemNumber(items.Phone);
+                setItemAddress(items.Address);
             })
     }, []);
 
     const onSubmit = async e => {
         e.preventDefault();
-        updateCompany(id, company);
+        updateCompany(id, { itemName, itemNumber, itemAddress });
         history.push("/ViewCompany");
     };
 
@@ -43,20 +37,20 @@ function EditCompany({ setAlert }) {
                             <div className="card-body">
                                 <div className="form-group">
                                     <label for="exampleInputName"> Name</label>
-                                    <input type="text" className="form-control" id="exampleInputName" placeholder="Enter Name" name="name" defaultValue={name}
-                                        onChange={e => onInputChange(e)} />
+                                    <input type="text" className="form-control" id="exampleInputName" placeholder="Enter Name" name="name" defaultValue={itemName}
+                                        onChange={event => setItemName(event.target.value)} />
                                 </div>
                                 <div className="form-group">
                                     <label for="exampleInputCode">Phone</label>
-                                    <input type="text" className="form-control" id="exampleInputCode" placeholder="Enter Phone Number" name="number" defaultValue={number}
-                                        onChange={e => onInputChange(e)} />
+                                    <input type="text" className="form-control" id="exampleInputCode" placeholder="Enter Phone Number" name="number" defaultValue={itemNumber}
+                                        onChange={event => setItemNumber(event.target.value)} />
                                 </div>
                                 <div className="form-group">
                                     <label for="exampleInputCode">Address</label>
                                     <input type="text" className="form-control" id="exampleInputCode" placeholder="Enter Address"
                                         name="address"
-                                        defaultValue={address}
-                                        onChange={e => onInputChange(e)}
+                                        defaultValue={itemAddress}
+                                        onChange={event => setItemAddress(event.target.value)}
                                     />
                                 </div>
 
