27f3df1affb9a00115f6cabab3bd57608ee4004c
mac window <mac.window.10@gmail.com>
2021-02-28 18:35:53 +0500

    start end date

--- basic_app/src/views/App/Breadcrumb.js
+++ basic_app/src/views/App/Breadcrumb.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
 function Breadcrumb() {
-  
+
   return (
     <div className="content-header">
-    <div className="container-fluid">
-      <div className="row mb-2">
+      <div className="container-fluid">
+        <div className="row mb-2">
+          {/*
         <div className="col-sm-6">
           <h1 className="m-0">Dashboard</h1>
         </div>
+        
         <div className="col-sm-6">
           <ol className="breadcrumb float-sm-right">
             <li className="breadcrumb-item"><a href="#">Home</a></li>
             <li className="breadcrumb-item active">Dashboard</li>
           </ol>
         </div>
+          */}
+        </div>
       </div>
     </div>
-  </div>
-  
+
   );
 }
 
