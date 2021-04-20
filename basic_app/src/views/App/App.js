80f255d2aecefb589d21efb9e806cea1c2dddc63
mac window <mac.window.10@gmail.com>
2021-04-20 23:05:11 +0500

    react ErrorBoundary

--- basic_app/src/views/App/App.js
+++ basic_app/src/views/App/App.js
@@ -8,7 +8,7 @@ import Content from './Content';
 import Footer from './Footer';
 import Header from './Header';
 import Breadcrumb from './Breadcrumb';
-
+import ErrorBoundary from './ErrorBoundary'
 
 function App() {
 
@@ -26,6 +26,11 @@ function App() {
         <Breadcrumb></Breadcrumb>
         <Content></Content>
       </div>
+
+      <ErrorBoundary>
+        <p>Some error occurred!</p>
+      </ErrorBoundary>
+      
       <Footer></Footer>
     </BrowserRouter>
   );
